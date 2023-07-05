17db71572e40d9a9f5d0c2173f791d3236a0eac6
Jhamtan <Jhamtan@outlook.com>
2023-07-05 22:18:20 +0800

    basic

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,28 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+import { items } from './load-data';
+
+const wordCompletionItems = items;
+let DOCUMENT_SELECTOR: Array<string> = ['markdown'];
+
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-english-word-hint" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-english-word-hint.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown English Word Hint!');
+	const provide = vscode.languages.registerCompletionItemProvider(DOCUMENT_SELECTOR, {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+			let text = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position));
+			let pattern = /^.*?\b(\w*)$/;
+			let prefix = (text.match(pattern) || ['', ''])[1];
+			if (!prefix) { return null; }
+			return wordCompletionItems.filter(item => item.filterText?.startsWith(prefix));
+		},
+		resolveCompletionItem(item: vscode.CompletionItem) {
+			return item;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(provide);
 }
 
 // This method is called when your extension is deactivated
