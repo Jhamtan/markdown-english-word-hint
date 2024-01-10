195bc4552c00264283d14115559cc8319a3d101a
Jhamtan <Jhamtan@outlook.com>
2024-01-10 18:09:38 +0800

    fix: correct word phonetics

--- src/load-data.ts
+++ src/load-data.ts
@@ -8,14 +8,12 @@ interface Word {
 
 export function getCompletionItem(): HintWord[] {
   let completionItems: Array<HintWord> = [];
-  for (let i = 0; i < 11; i++) {
-    const dictionary: Array<Word> = require(`./data/dictionary${i}.json`);
-    dictionary.forEach(word => {
-      let item = new vscode.CompletionItem(word.word, vscode.CompletionItemKind.Enum);
-      item.filterText = word.word;
-      item.detail = word.detail;
-      completionItems.push(item);
-    });
-  }
+  const dictionary: Array<Word> = require('./data/dictionary.json');
+  dictionary.forEach(word => {
+    let item = new vscode.CompletionItem(word.word, vscode.CompletionItemKind.Enum);
+    item.filterText = word.word;
+    item.detail = word.detail;
+    completionItems.push(item);
+  });
   return completionItems;
 }
